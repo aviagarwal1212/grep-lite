bdfcf568064318df6c0de9f267f94f989dd5f75c
Avi Agarwal <aviagarwal1212@gmail.com>
2023-11-06 12:42:36 +0000

    add surrounding lines

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 fn main() {
-    let search_term = "picture";
-    let quote = "\
-Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books.
-What do we seek through millions of pages?";
+    let ctx_lines = 2;
+    let needle = "oo";
 
-    for (i, line) in quote.lines().enumerate() {
-        if line.contains(search_term) {
-            println!("{}: {}", i + 1, line);
+    let haystack = "\
+Every face, every shop, 
+bedroom window, public-house, and
+dark square is a picture 
+feverishly turned--in search of what?
+It is the same with books.
+What do we seek 
+through millions of pages?";
+
+    let lines = haystack.lines().enumerate().collect::<Vec<(usize, &str)>>();
+    let chunk = lines
+        .windows(2 * ctx_lines + 1)
+        .find(|&lines| lines[ctx_lines].1.contains(needle))
+        .unwrap();
+
+    for (i, item) in chunk.iter().enumerate() {
+        if i == ctx_lines {
+            println!("* {} {}", item.0, item.1);
+        } else {
+            println!("  {} {}", item.0, item.1);
         }
     }
 }
