a8f7b52464c92d623a5e3e1946547c617bc535ab
Avi Agarwal <aviagarwal1212@gmail.com>
2023-11-10 17:19:51 +0000

    handle files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::cmp::max;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 use clap::{arg, Parser};
 use regex::Regex;
@@ -6,49 +9,26 @@ use regex::Regex;
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Args {
-    /// Pattern to search in haystack
-    #[arg(short, long)]
+    /// Pattern to search for
+    #[arg(short, long, required = false, default_value_t = String::from("book"))]
     pattern: String,
-    /// Number of rows to show for context in each direction
-    #[arg(short, long, default_value_t = 2)]
-    context: usize,
+    /// File to search in
+    #[arg(short, long, required = false, default_value_t = String::from("haystack.txt"))]
+    input: String,
 }
 
 fn main() {
     let args = Args::parse();
-    let ctx_lines: usize = args.context;
     let needle = Regex::new(&args.pattern).unwrap();
+    let input = args.input;
 
-    let haystack = "\
-Every face, every shop, 
-bedroom window, public-house, and
-dark square is a picture 
-feverishly turned--in search of what?
-It is the same with books.
-What do we seek 
-through millions of pages?";
-
-    let queried_idx = haystack
+    let f = File::open(input).unwrap();
+    let (idx, line) = BufReader::new(f)
         .lines()
-        .position(|line| needle.is_match(line))
+        .map_while(|line| line.ok())
+        .enumerate()
+        .find(|(_idx, line)| needle.is_match(line))
         .unwrap();
 
-    let start_index = max(queried_idx as isize - ctx_lines as isize, 0) as usize;
-
-    let output_chunk = haystack
-        .lines()
-        .skip(start_index)
-        .take(queried_idx - start_index + ctx_lines + 1)
-        .enumerate()
-        .map(|(idx, item)| {
-            let line_num = idx + start_index + 1;
-            if line_num == queried_idx + 1 {
-                return format!("* {} {}", line_num, item);
-            }
-            format!("  {} {}", line_num, item)
-        })
-        .collect::<Vec<_>>()
-        .join("\n");
-
-    println!("{}", output_chunk);
+    println!("{}: {}", idx + 1, line);
 }
